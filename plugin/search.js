d78f92c48d61c23c46b74c057b5ef68d119f0739
ervandew <ervandew@gmail.com>
2008-11-21 23:37:48 -0800

    updated the search command to be more generic, making adding more engines easier.  added support for searching web mail clients

--- plugin/search.js
+++ plugin/search.js
@@ -1,6 +1,7 @@
 /**
  * Plugin to search the current domain using your default search engine
- * (currently supports google and yahoo).
+ * (currently supports google and yahoo).  Also supports searching from your
+ * web mail account (currently limited to gmail and yahoo mail).
  *
  * Usage:
  *   :search <search terms>
@@ -9,54 +10,138 @@
  * command will attempt to determine the domain to search by examining the
  * current search query.
  *
- * TODO
- *   - add support for using other search engines
+ * Example mapping similar to 'o' or 't' to start the search command by typing
+ * 's' in normal mode:
  *
- * @author Eric Van Dewoetine
+ *   map s :search<space>
+ *
+ * TODO
+ *   - add support for using other search engines / web mail clients
+ *
+ * @author Eric Van Dewoetine (ervandew@gmail.com)
  * @version 0.1
  */
 
 commands.add(["search"],
   "Search the current site using a search engine.",
   function(args) {
-    var supported = ['google', 'yahoo'];
-    var defsearch = options['defsearch'];
     var loc = window.content.document.location.toString();
     var domain = loc.replace(/[a-z]+:\/\/(.*?)\/.*/, '$1');
 
-    // if we are on the search results page for the search engine, grab the
-    // site from the search query if we can.
-    if (new RegExp('^(www\.|search\.)?' + defsearch + '.com$').test(domain)){
-      var value;
-      if (defsearch == 'google'){
-        var form = window.content.document.getElementById('tsf');
-        for each (input in form.elements){
-          if (input.name == 'q'){
-            value = input.value;
-            break;
-          }
-        }
-      }else if (defsearch == 'yahoo'){
-        value = window.content.document.getElementById('yschsp').value;
-      }
-
-      var match = value ? /site:\s*(\S+).*/.exec(value) : null;
-      if (match){
-        domain = match[1];
-      }
+    // alternate flow for searching mail
+    var mail = emailDomains.find(domain);
+    if (mail){
+      mail.search(args.string);
+      return;
     }
 
-    if (supported.indexOf(defsearch) == -1){
+    var defsearch = options['defsearch'];
+    var engine = searchDomains[defsearch];
+    if (!engine){
+      var engines = [];
+      for (var name in searchDomains){
+        if (searchDomains.hasOwnProperty(name))
+          engines.push(name);
+      }
       liberator.echoerr(
-        ":search currently only supports defsearch engines 'google' and 'yahoo'"
-      );
-    }else{
-      events.feedkeys(':open ' + defsearch + ' site:' + domain + ' ' + args.string + '<cr>');
+        ":search currently only supports the following defsearch engines: " +
+          engines.join(", "));
+      return;
     }
+
+    // if we are on the search results page for the search engine, attempt to
+    // grab the domain from the search query.
+    if (engine.urlPattern.test(domain)){
+      domain = engine.searchInputDomain() || domain;
+    }
+
+    var site = engine.domainFilter ? engin.domainFilter(domain) : 'site:' + domain;
+    events.feedkeys(
+      ':open ' + defsearch + ' ' + site + ' ' + args.string + '<cr>');
   }
 );
 
-mappings.add([modes.NORMAL], ["s"],
-  "Initiate the :search command",
-  function () { commandline.open(":", "search ", modes.EX); }
-);
+const searchDomains = {
+  google: {
+    urlPattern: new RegExp('^www\.google\.com$'),
+
+    searchInputDomain: function(){
+      var value = null;
+      var form = window.content.document.getElementById('tsf');
+      for each (input in form.elements){
+        if (input.name == 'q'){
+          value = input.value;
+          break;
+        }
+      }
+      var match = value ? /site:\s*(\S+).*/.exec(value) : null;
+      return match ? match[1] : null;
+    }
+  },
+
+  yahoo: {
+    urlPattern: new RegExp('^search\.yahoo\.com$'),
+
+    searchInputDomain: function(){
+      var value = window.content.document.getElementById('yschsp').value;
+      var match = value ? /site:\s*(\S+).*/.exec(value) : null;
+      return match ? match[1] : null;
+    }
+  }
+};
+
+const emailDomains = {
+  find: function(domain){
+    for (var name in emailDomains){
+      if (emailDomains.hasOwnProperty(name) && emailDomains[name].urlPattern){
+        if (emailDomains[name].urlPattern.test(domain)){
+          return emailDomains[name];
+        }
+      }
+    }
+  },
+
+  gmail: {
+    urlPattern: new RegExp('^mail\.google\.com$'),
+
+    search: function(query){
+      var loc = window.content.document.location.toString();
+      var domain = loc.replace(/(https?:\/\/.*?)\/.*/, '$1');
+      events.feedkeys(
+        ':open ' + domain + '/mail/#search/' + escape(query) + '<cr>');
+    }
+  },
+
+  yahoo: {
+    urlPattern: new RegExp('^.*\.mail\.yahoo\.com$'),
+
+    search: function(query){
+      var form =
+        window.content.document.getElementById('searchTheMailFrmtop') ||
+        window.content.document.getElementById('searchformtop');
+
+      // "classic"
+      if (form){
+        for each (input in form.elements){
+          if (input.name == 's'){
+            input.value = query;
+            break;
+          }
+        }
+        form.submit();
+
+      // "new"
+      }else{
+        var input = window.content.document.getElementById('_test_search_input');
+        var event = window.content.document.createEvent('KeyboardEvent');
+        event.initKeyEvent(
+          "keydown", true, true, null,
+          false, false, false, false, 13, 0);
+        input.focus();
+        input.value = query;
+        input.dispatchEvent(event);
+        input.blur();
+      }
+    }
+  }
+};
