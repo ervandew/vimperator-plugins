30d075c5b85e878e2734bb7364f43fadb82d6a53
ervandew <ervandew@gmail.com>
2009-12-08 10:57:22 -0800

    update readablity, use newspaper style

--- plugin/readability.js
+++ plugin/readability.js
@@ -19,36 +19,35 @@ commands.add(["readability"],
     // document.location
     window.content.document.location = "javascript:" +
       "(function(){" +
-      "    doc = window.content.document;" +
-      "    readStyle = 'style-newspaper';" +
-      "    readSize = 'size-large';" +
-      "    readMargin = 'margin-narrow';" +
-      "    _readability_script = doc.createElement('SCRIPT');" +
-      "    _readability_script.type = 'text/javascript';" +
-      "    _readability_script.src =" +
-      "      'http://lab.arc90.com/experiments/readability/js/readability.js?x=' + (Math.random());" +
-      "    doc.getElementsByTagName('head')[0].appendChild(_readability_script);" +
-      "    _readability_css = doc.createElement('LINK');" +
-      "    _readability_css.rel = 'stylesheet';" +
-      "    _readability_css.href =" +
+      "    readStyle='style-ebook';" +
+      "    readSize='size-large';" +
+      "    readMargin='margin-narrow';" +
+      "    _readability_script=document.createElement('SCRIPT');" +
+      "    _readability_script.type='text/javascript';" +
+      "    _readability_script.src=" +
+      "      'http://lab.arc90.com/experiments/readability/js/readability.js?x='+(Math.random());" +
+      "    document.getElementsByTagName('head')[0].appendChild(_readability_script);" +
+      "    _readability_css=document.createElement('LINK');" +
+      "    _readability_css.rel='stylesheet';" +
+      "    _readability_css.href=" +
       "      'http://lab.arc90.com/experiments/readability/css/readability.css';" +
-      "    _readability_css.type = 'text/css';" +
-      "    _readability_css.media = 'screen';" +
-      "    doc.getElementsByTagName('head')[0].appendChild(_readability_css);" +
-      "    _readability_print_css = doc.createElement('LINK');" +
-      "    _readability_print_css.rel = 'stylesheet';" +
-      "    _readability_print_css.href =" +
+      "    _readability_css.type='text/css';" +
+      "    _readability_css.media='screen';" +
+      "    document.getElementsByTagName('head')[0].appendChild(_readability_css);" +
+      "    _readability_print_css=document.createElement('LINK');" +
+      "    _readability_print_css.rel='stylesheet';" +
+      "    _readability_print_css.href=" +
       "      'http://lab.arc90.com/experiments/readability/css/readability-print.css';" +
-      "    _readability_print_css.media = 'print';" +
-      "    _readability_print_css.type = 'text/css';" +
-      "    doc.getElementsByTagName('head')[0].appendChild(_readability_print_css);" +
+      "    _readability_print_css.media='print';" +
+      "    _readability_print_css.type='text/css';" +
+      "    document.getElementsByTagName('head')[0].appendChild(_readability_print_css);" +
       "    setTimeout(function(){" +
-      "      var overlay = doc.body;" +
+      "      var overlay = document.body;" +
       "      if (overlay) {" +
       "        overlay.className = '';" +
       "        overlay.firstChild.className = '';" +
-      "        overlay.style.backgroundColor = '#ccc';" +
-      "        overlay.style.color = '#444';" +
+      "        overlay.style.backgroundColor = '#999';" +
+      "        overlay.style.color = '#333';" +
       "      }" +
       "    }, 1000);" +
       "})();";
